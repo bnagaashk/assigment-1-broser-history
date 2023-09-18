e887dd2b9eea333d2439d491f9e298ebfbcdf065
bnagaashok <bnagaashok@gmail.com>
2023-09-18 16:10:36 +0000

    commit message

---
+++ src/components/BrowserHistory/index.js
@@ -0,0 +1,37 @@
+import './index.css'
+
+const BrowserHistory = props => {
+  const {eachBrowser, deleteItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = eachBrowser
+
+  const onDelete = () => {
+    deleteItem(id)
+  }
+  return (
+    <li>
+      <div className="browser-history-item-container">
+        <h1 className="time">{timeAccessed}</h1>
+
+        <div className="website-con">
+          <img src={logoUrl} className="logo" alt="domain logo" />
+          <h1 className="title">{title}</h1>
+          <p className="website-name">{domainUrl}</p>
+        </div>
+        <button
+          type="button"
+          className="button"
+          onClick={onDelete}
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+            className="delete-icon"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BrowserHistory
---
+++ src/components/EachBroserHistory/index.js
@@ -0,0 +1,79 @@
+import {Component} from 'react'
+import BrowserHistory from '../BrowserHistory'
+
+import './index.css'
+
+class EachBroserHistory extends Component {
+  constructor(props) {
+    super(props)
+    const {initialHistoryList} = props
+    this.state = {intialUsersList: initialHistoryList, searchInput: ''}
+  }
+
+  onchangeInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  deleteItem = id => {
+    const {intialUsersList} = this.state
+    const deleteditemsList = intialUsersList.filter(
+      eachItem => eachItem.id !== id,
+    )
+
+    this.setState({intialUsersList: deleteditemsList})
+  }
+
+  render() {
+    const {intialUsersList, searchInput} = this.state
+    const filteredItemsList = intialUsersList.filter(eachItem =>
+      eachItem.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    const res = filteredItemsList.every(each => each === true)
+    let a = true
+    if (res || filteredItemsList.length === 0) {
+      a = false
+    }
+    return (
+      <div className="browser-history-con">
+        <div className="navBarsection">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+            alt="app logo"
+            className="history-logo"
+          />
+          <div className="search-con">
+            <button type="button" className="search-button">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+                alt="search-logo"
+              />
+            </button>
+            <input
+              type="text"
+              className="input-ele"
+              placeholder="Search history"
+              onChange={this.onchangeInput}
+              value="search"
+            />
+          </div>
+        </div>
+        <div className="broser-items-container">
+          <ul>
+            {a ? (
+              filteredItemsList.map(eachItem => (
+                <BrowserHistory
+                  eachBrowser={eachItem}
+                  key={eachItem.id}
+                  deleteItem={this.deleteItem}
+                />
+              ))
+            ) : (
+              <p className="no-content">There is no history to show </p>
+            )}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default EachBroserHistory
